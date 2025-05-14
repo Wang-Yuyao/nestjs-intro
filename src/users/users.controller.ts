5ca2f50b3e2575f484e8d2de2e3e47983bf9f4ca
Wang-Yuyao <wangyuyao19960205@163.com>
2025-05-14 22:41:23 +0900

    feat dto実装、Pipe実装、decorators実装、validation実装

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+  ParseIntPipe,
+  DefaultValuePipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,14 +21,33 @@ export class UsersController {
   }
 
   @Get('/:id')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log('id:', typeof id);
+    console.log('limit:', typeof limit);
+
+    console.log('page:', typeof page);
+    return 'You sent a GET request to users endpoint';
+  }
+  @Get('/:id')
   public getUserById(@Param() params: any, @Query() query: any) {
     console.log(params, query);
     return `You have sent a GET request to endpoints`;
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
-    console.log(request);
+  public createUsers(
+    @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(createUserDto);
+    console.log(headers);
+    console.log(ip);
+
     return 'You have sent a post request to users endpoint';
   }
 }
