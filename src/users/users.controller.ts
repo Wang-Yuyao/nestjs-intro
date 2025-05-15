802bd8da6c0edd62495299523b64227f3e4ba3f3
Wang-Yuyao <wangyuyao19960205@163.com>
2025-05-15 22:11:26 +0900

    feat: service 実装

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,11 +10,22 @@ import {
   ParseIntPipe,
   DefaultValuePipe,
   ValidationPipe,
+  Patch,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamsDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    // Injecting Users Service
+    private readonly usersService: UsersService,
+  )
+  {}
+
+
   @Get()
   public getAllUsers() {
     return 'You have sent a GET request to /users (no ID)';
@@ -22,14 +33,14 @@ export class UsersController {
 
   @Get('/:id')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number,
+    @Param() getUserParamDto: GetUsersParamsDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log('id:', typeof id);
-    console.log('limit:', typeof limit);
+    console.log('limit:', limit);
+    console.log('getUserParamDto:', getUserParamDto);
 
-    console.log('page:', typeof page);
+    console.log('page:', page);
     return 'You sent a GET request to users endpoint';
   }
   @Get('/:id')
@@ -40,14 +51,19 @@ export class UsersController {
 
   @Post()
   public createUsers(
-    @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+    @Body() createUserDto: CreateUserDto,
     @Headers() headers: any,
     @Ip() ip: any,
   ) {
-    console.log(createUserDto);
+    console.log(createUserDto instanceof CreateUserDto);
     console.log(headers);
     console.log(ip);
 
     return 'You have sent a post request to users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto){
+    return patchUserDto;
   }
 }
