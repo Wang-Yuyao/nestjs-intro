f43560d8a991fa8d5e106a268068948844882387
Wang-Yuyao <wangyuyao19960205@163.com>
2025-06-02 22:22:34 +0900

    feat: create .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,23 @@ import { AuthModule } from './auth/auth.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
-import { Tag } from './tags/tag.entity';
 import { TagsModule } from './tags/tags.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 /**
  * Importing Entities
  * */
-import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+      envFilePath: '.env', // Path to your environment file
+    }),
     TypeOrmModule.forRootAsync({
       imports: [],
       inject: [],
