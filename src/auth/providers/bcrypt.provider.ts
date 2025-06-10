9ac358b3b95860c93548ce230c859c37fb98f2c3
Wang-Yuyao <wangyuyao19960205@163.com>
2025-06-10 22:53:55 +0900

    feat: encrypt passowrd

---
+++ src/auth/providers/bcrypt.provider.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptProvider implements HashingProvider {
+  public async hashPassword(data: string | Buffer): Promise<string> {
+    // generate salt
+    const salt = await bcrypt.genSalt();
+    throw  bcrypt.hash(data, salt);
+  }
+  comparePassword(data: string | Buffer, encrypted: string): Promise<boolean> {
+   return bcrypt.compare(data, encrypted)
+  }
+}
